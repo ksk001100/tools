bd6edc228c2b09c27cceb40c0d46a6e9909f922e
ksk001100 <hm.pudding0715@gmail.com>
2020-07-18 06:42:10 +0900

    fix

--- src/utils.py
+++ src/utils.py
@@ -7,13 +7,53 @@ import subprocess
 import atexit
 import time
 
-def set_socks5():
-    socks.set_default_proxy(socks.PROXY_TYPE_SOCKS5, '127.0.0.1', 9050)
-    socket.socket = socks.socksocket
+class Onion:
+    proc = None
 
-def get_global_ip():
-    res = json.loads(urlopen('https://api.ipify.org?format=json'))
-    return res['ip']
+    def __init__(self):
+        self.start_tor()
+        self.set_socks5()
+        atexit.register(self.stop_tor)
+
+    def __del__(self):
+        self.stop_tor()
+
+    def set_socks5(self):
+        socks.set_default_proxy(socks.PROXY_TYPE_SOCKS5, '127.0.0.1', 9050)
+        socket.socket = socks.socksocket
+
+    def start_tor(self):
+        self.proc = subprocess.Popen('tor', stdout=subprocess.DEVNULL)
+    
+    def stop_tor(self):
+        self.proc.kill()
+
+    def restart_tor(self):
+        self.stop_tor()
+        self.start_tor()
+        time.sleep(5)
+
+
+class Request:
+    onion = None
+
+    def __init__(self, is_onion=False):
+        if is_onion:
+            self.onion = Onion()
+
+    def get_global_ip(self):
+        res = json.loads(self.get('https://api.ipify.org', {'format': 'json'}))
+        return res['ip']
+
+    def get(self, url, params={}):
+        q = ''
+        if len(params):
+            q = '?' + '&'.join(map(lambda x: x[0] + '=' + x[1], params.items()))
+        return urlopen(url + q).read().decode('utf8')
+
+    def post(self, url, params={}):
+        pass
+
 
 def generate_passwords(patterns=[]):
     passwords = []
