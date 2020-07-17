5e57986b44c13a6a96b4f44d739086f377638cd3
ksk001100 <hm.pudding0715@gmail.com>
2020-07-17 22:37:22 +0900

    first commit

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import itertools
 import socks
 import socket
+import json
+from urllib.request import urlopen
 import subprocess
 import atexit
 import time
@@ -9,6 +11,9 @@ def set_socks5():
     socks.set_default_proxy(socks.PROXY_TYPE_SOCKS5, '127.0.0.1', 9050)
     socket.socket = socks.socksocket
 
+def get_global_ip():
+    res = json.loads(urlopen('https://api.ipify.org?format=json'))
+    return res['ip']
 
 def generate_passwords(patterns=[]):
     passwords = []
